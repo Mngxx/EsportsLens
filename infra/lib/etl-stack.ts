74223360a6ce9de491a49262802ef941797b750b
Ron Lara <lararon2428@gmail.com>
2026-08-20 00:23:14 +0800

    feat: provision glue database and glue job role in preparation for the 2 glue jobs

--- infra/lib/etl-stack.ts
+++ infra/lib/etl-stack.ts
@@ -1,3 +1,5 @@
+import * as glue_alpha from "@aws-cdk/aws-glue-alpha";
+import * as iam from "aws-cdk-lib/aws-iam";
 import type * as s3 from "aws-cdk-lib/aws-s3";
 import * as cdk from "aws-cdk-lib/core";
 import type { Construct } from "constructs";
@@ -10,5 +12,24 @@ export interface EtlStackProps extends cdk.StackProps {
 export class EtlStack extends cdk.Stack {
 	constructor(scope: Construct, id: string, props: EtlStackProps) {
 		super(scope, id, props);
+
+		//Glue Database
+		const database = new glue_alpha.Database(this, "EsportsLensDatabase", {
+			databaseName: "esportslens_db",
+			removalPolicy: cdk.RemovalPolicy.DESTROY,
+		});
+
+		//IAM role shared by both Glue jobs
+		const glueJobRole = new iam.Role(this, "GlueJobRole", {
+			assumedBy: new iam.ServicePrincipal("glue.amazonaws.com"),
+			managedPolicies: [
+				iam.ManagedPolicy.fromAwsManagedPolicyName(
+					"service-role/AWSGlueServiceRole",
+				),
+			],
+		});
+
+		props.rawBucket.grantRead(glueJobRole);
+		props.curatedBucket.grantReadWrite(glueJobRole);
 	}
 }
