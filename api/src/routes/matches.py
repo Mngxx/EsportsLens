9ad0e773cb08af65563161c7407e1c73f027757e
Mngxx <lararon2428@gmail.com>
2026-09-02 02:06:17 +0800

    fix: unnecessary async and await in both routes scripts and replaced Path for the pattern

--- api/src/routes/matches.py
+++ api/src/routes/matches.py
@@ -6,14 +6,14 @@ router = APIRouter(prefix="/matches", tags=["matches"])
 
 
 @router.get("/dota2/{match_id}", response_model=list[Dota2MatchSchema])
-async def get_dota2_matches(match_id: int) -> list[Dota2MatchSchema]:
+def get_dota2_matches(match_id: int) -> list[Dota2MatchSchema]:
     sql = f"SELECT * FROM dota2_matches WHERE match_id = {match_id}"
-    dota2_matches = await run_query(sql)
+    dota2_matches = run_query(sql)
     return dota2_matches
 
 
 @router.get("/lol/{match_id}", response_model=list[LoLMatchSchema])
-async def get_lol_matches(match_id: int) -> list[LoLMatchSchema]:
+def get_lol_matches(match_id: int) -> list[LoLMatchSchema]:
     sql = f"SELECT * FROM league_of_legends_matches WHERE match_id = {match_id}"
-    lol_matches = await run_query(sql)
+    lol_matches = run_query(sql)
     return lol_matches
--- api/src/routes/meta.py
+++ api/src/routes/meta.py
@@ -11,28 +11,28 @@ router = APIRouter(prefix="/meta", tags=["meta"])
 
 
 @router.get("/dota2/heroes", response_model=list[Dota2HeroSchema])
-async def get_dota2_heroes() -> list[Dota2HeroSchema]:
+def get_dota2_heroes() -> list[Dota2HeroSchema]:
     sql = "SELECT * FROM dota2_heroes ORDER BY name ASC"
-    dota2_heroes = await run_query(sql)
+    dota2_heroes = run_query(sql)
     return dota2_heroes
 
 
 @router.get("/dota2/heroes/stats", response_model=list[Dota2HeroStatsSchema])
-async def get_dota2_heroes_stats() -> list[Dota2HeroStatsSchema]:
+def get_dota2_heroes_stats() -> list[Dota2HeroStatsSchema]:
     sql = "SELECT * FROM dota2_hero_stats ORDER BY hero_name ASC"
-    dota2_heroes_stats = await run_query(sql)
+    dota2_heroes_stats = run_query(sql)
     return dota2_heroes_stats
 
 
 @router.get("/lol/champions", response_model=list[LoLChampionSchema])
-async def get_lol_champions() -> list[LoLChampionSchema]:
+def get_lol_champions() -> list[LoLChampionSchema]:
     sql = "SELECT * FROM league_of_legends_champions ORDER BY name ASC"
-    lol_champions = await run_query(sql)
+    lol_champions = run_query(sql)
     return lol_champions
 
 
 @router.get("/lol/champions/stats", response_model=list[LoLChampionStatsSchema])
-async def get_lol_champions_stats() -> list[LoLChampionStatsSchema]:
+def get_lol_champions_stats() -> list[LoLChampionStatsSchema]:
     sql = "SELECT * FROM league_of_legends_champion_stats ORDER BY champion_name ASC"
-    lol_champions_stats = await run_query(sql)
+    lol_champions_stats = run_query(sql)
     return lol_champions_stats
