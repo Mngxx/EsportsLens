ce854fb5ec1c5f5993b497d947218ef033bc40f9
Mngxx <lararon2428@gmail.com>
2026-09-02 02:34:12 +0800

    fix: match_id replace to str the same in the Glue table
    - validated in Postman along with all of the routes fetch
    - applied same rule in match_id to add Path(pattern=regex) validation to
    avoid SQL injection

--- api/src/routes/matches.py
+++ api/src/routes/matches.py
@@ -1,5 +1,5 @@
 from src.models.schemas import Dota2MatchSchema, LoLMatchSchema
-from fastapi import APIRouter
+from fastapi import APIRouter, Path
 from src.db.athena import run_query
 
 router = APIRouter(prefix="/matches", tags=["matches"])
@@ -13,7 +13,9 @@ def get_dota2_matches(match_id: int) -> list[Dota2MatchSchema]:
 
 
 @router.get("/lol/{match_id}", response_model=list[LoLMatchSchema])
-def get_lol_matches(match_id: int) -> list[LoLMatchSchema]:
-    sql = f"SELECT * FROM league_of_legends_matches WHERE match_id = {match_id}"
+def get_lol_matches(
+    match_id: str = Path(pattern=r"^[A-Za-z0-9_-]+$"),
+) -> list[LoLMatchSchema]:
+    sql = f"SELECT * FROM league_of_legends_matches WHERE match_id = '{match_id}'"
     lol_matches = run_query(sql)
     return lol_matches
