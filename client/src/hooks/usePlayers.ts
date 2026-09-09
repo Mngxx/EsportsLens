e53cc10e4929121624a463fc0859ed5f3e32f203
Mngxx <lararon2428@gmail.com>
2026-09-10 01:48:42 +0800

    feat: added usePlayers hook to wrap api call for the components

---
+++ client/src/hooks/usePlayers.ts
@@ -0,0 +1,21 @@
+import { searchDota2Players, searchLoLPlayers } from "../lib/api";
+import { useApi } from "./useApi";
+import type {
+    Game,
+    Dota2PlayerSearchResult,
+    LoLPlayerSearchResult,
+} from "../types";
+
+export function usePlayerSearch(
+    game: Game,
+    name: string,
+    limit?: number,
+): Dota2PlayerSearchResult[] | LoLPlayerSearchResult[] {
+    return useApi(
+        () =>
+            game === "dota2"
+                ? searchDota2Players(name, limit)
+                : searchLoLPlayers(name, limit),
+        [game, name, limit],
+    );
+}
