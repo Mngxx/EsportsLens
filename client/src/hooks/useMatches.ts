be33b2779065ff61c0cf0a5d067cb18530331171
Mngxx <lararon2428@gmail.com>
2026-09-12 23:49:00 +0800

    feat: modified hook to accommodate the recent matches fetch in api

--- client/src/hooks/useMatches.ts
+++ client/src/hooks/useMatches.ts
@@ -1,6 +1,19 @@
-import { getDota2PlayerMatches, getLoLPlayerMatches } from "../lib/api";
+import {
+    getDota2Match,
+    getDota2PlayerMatches,
+    getLoLMatch,
+    getLoLPlayerMatches,
+    getRecentDota2Matches,
+    getRecentLoLMatches,
+} from "../lib/api";
 import { useApi } from "./useApi";
-import type { Game, Dota2Match, LoLMatch } from "../types";
+import type {
+    Dota2Match,
+    Dota2MatchSummary,
+    Game,
+    LoLMatch,
+    LoLMatchSummary,
+} from "../types";
 
 export function usePlayerMatches(
     game: Game,
@@ -15,5 +28,30 @@ export function usePlayerMatches(
                 : getLoLPlayerMatches(id as string, limit),
         [game, id, limit],
         enabled,
+    );
+}
+
+export function useMatch(
+    game: Game,
+    matchId: string | number,
+    enabled: boolean = true,
+) {
+    return useApi<Dota2Match[] | LoLMatch[]>(
+        () =>
+            game === "dota2"
+                ? getDota2Match(matchId as number)
+                : getLoLMatch(matchId as string),
+        [game, matchId],
+        enabled,
+    );
+}
+
+export function useRecentMatches(game: Game, limit?: number) {
+    return useApi<Dota2MatchSummary[] | LoLMatchSummary[]>(
+        () =>
+            game === "dota2"
+                ? getRecentDota2Matches(limit)
+                : getRecentLoLMatches(limit),
+        [game, limit],
     );
 }
